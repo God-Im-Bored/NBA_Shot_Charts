cb94dc69e2a387cfe5146ff6f1b4937aaedada8a
Aram <aram_martin@alumni.brown.edu>
2021-04-09 13:05:52 -0500

    FIX: shot chart detail api call/ fix string argument to include %20 b/w names

--- api/api.py
+++ api/api.py
@@ -15,40 +15,35 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+
 @app.route('/', methods=["GET"])
 def index():
     return 'Flask API Index. Sup Bubba'
 
+
 @app.route('/players', methods=['GET'])
 def players():
-    return { 'data': player_list}
+    return {'data': player_list}
+
 
 @app.route('/teams', methods=['GET'])
 def teams():
     return {'data': team_list}
 
+
 @app.route('/player_info/<player_name>', methods=['GET'])
 def player_info(player_name):
-    response = shotchartdetail.ShotChartDetail(
+    for player in player_list:
+        if(player_name == player['full_name']):
+            response = shotchartdetail.ShotChartDetail(
                 team_id=0,
-                player_id=2544
+                player_id=player['id']
             )
-    player_data = json.loads(response.get_json())
-
-    return {'data': player_data}
-
-    # for player in player_list:
-    #     if(name == player['full_name']):
-    #         response = shotchartdetail.ShotChartDetail(
-    #             team_id=0,
-    #             player_id=2544
-    #         )
-    #     player_data = json.loads(response.get_json())
-
-    # return {'player_data': player_data}
-
-
-
+            player_data = json.loads(response.get_json())
+            print(player_data) 
+            print(player_name)
+    return {'player_data': player_data}
+            # return {'player_data': player_data}
 
 if __name__ == '__main__':
     app.run(debug=True)
--- client/components/PlayerSearch.js
+++ client/components/PlayerSearch.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from 'axios'
 import { TextField } from "@material-ui/core";
 import { Autocomplete } from '@material-ui/lab'
 import "regenerator-runtime/runtime";
@@ -10,11 +11,41 @@ const PlayerSearch = (players) => {
 
   const names = Array.isArray(data) ? data.map((player) => player.full_name) : "";
 
-  const handleChange =(event, name) => {
+  const handleChange = async (event, name) => {
     setValue(event.target.value)
     console.log(name)
     console.log(value)
+
+    if (event.key === 'Enter') {
+      try {
+        
+        // const result = await axios.get(`http://localhost:5000/player_info/${name}`)
+        // console.log(101, result.data)
+        console.log('Searched')
+    
+      } catch (error) {
+        console.error(error)
+      }
+    }
   }
+
+  // name = 'Kawhi Leonard'
+  // const search = async (event, name) => {
+  //   setValue(event.target.value)
+  //   console.log(name)
+  //   if (event.key === 'Enter') {
+  //     try {
+        
+  //       const result = await axios.get(`http://localhost:5000/player_info/${name}`)
+  //       console.log(101, result.data)
+  //       console.log('Searched')
+    
+  //     } catch (error) {
+  //       console.error(error)
+  //     }
+  //   }
+    
+  // }
 
   return (
     <div id="search-bar">
@@ -23,9 +54,9 @@ const PlayerSearch = (players) => {
       options={Array.isArray(names) ? names : []}
       getOptionLabel={(name) => name}
       onChange={handleChange}
-      autoComplete
+      onKeyPress={handleChange}
       clearOnEscape
-      renderInput={(params) => <TextField {...params} required label='Player' variant='outlined' />}
+      renderInput={(params) => <TextField {...params} required label='Player' variant='outlined'  />}
       style={{ width: 200 }}
       />
   </div>
