aabecb885d2260874ad2da3539a6e9c2c198933c
Aram <aram_martin@alumni.brown.edu>
2021-04-08 20:21:14 -0500

    FIX: player search bar fixed

--- client/components/PlayerSearch.js
+++ client/components/PlayerSearch.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { FormControl, TextField, MenuItem, InputLabel } from "@material-ui/core";
-import axios from "axios";
+import React, { useState } from "react";
+import { TextField } from "@material-ui/core";
+import { Autocomplete } from '@material-ui/lab'
 import "regenerator-runtime/runtime";
 
 const PlayerSearch = (players) => {
@@ -9,41 +9,25 @@ const PlayerSearch = (players) => {
   const { data } = playersList;
 
   const names = Array.isArray(data) ? data.map((player) => player.full_name) : "";
-  // console.log(777, names);
 
-  const handleClick = (e) => {
-    setValue(e.target.value)
-    console.log("You Clicked Me");
-  };
-
-  const handleChange = (e) => {
-    console.log("You changed me");
-  };
+  const handleChange =(event, name) => {
+    setValue(event.target.value)
+    console.log(name)
+    console.log(value)
+  }
 
   return (
     <div id="search-bar">
-    
-    <FormControl>
-      <TextField
-        select
-        id='player-select-list'
-        label='Player'
-        helperText='Please select a player'
-        SelectProps={{
-          multiple: true,
-          value: []
-        }}
-        onClick={handleClick}
-        >
-          {Array.isArray(names) ? (
-            names.map((name, index) => {
-              return <MenuItem key={index} value={name}>{name}</MenuItem>
-            })
-          ): (
-            'HELLO'
-          )}
-        </TextField>
-    </FormControl>
+      <Autocomplete
+      id='names-list'
+      options={Array.isArray(names) ? names : []}
+      getOptionLabel={(name) => name}
+      onChange={handleChange}
+      autoComplete
+      clearOnEscape
+      renderInput={(params) => <TextField {...params} required label='Player' variant='outlined' />}
+      style={{ width: 200 }}
+      />
   </div>
   );
 };
