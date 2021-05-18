5fe75ea86d8349dbc60c4993738211f462fd2923
Aram <aram_martin@alumni.brown.edu>
2021-05-17 21:26:10 -0500

    FIX: table conditional, maybe row/colum components

--- client/components/ShotChart.js
+++ client/components/ShotChart.js
@@ -1,57 +1,75 @@
-import React from 'react'
+import React from "react";
 
-import { Card, CardContent, CardHeader, Typography } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
+import { Card, CardContent, CardHeader, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
-    root: {
-        maxWidth: 400
-    }
-})
-
+  root: {
+    maxWidth: 400,
+  },
+});
 
 const ShotChart = (props) => {
-    const classes = useStyles()
-    
-    return (
-        props.data ? console.log(Object.entries(props.data)) : console.log('hi'),
-       
-        <div id='player-shot-chart-main'>
-            <Card className={classes.root}>
-                <CardHeader 
-                    title='Field Goal Breakdown'
-                    subheader='Player Shot Info'
-                />
-                <table>
-                    <thead>
-                        <tr>
-                           <th>Player</th>
-                           <th>League</th> 
-                        </tr>
-                        <tr>
-                            <th>Shot Value</th>
-                            <th>FGM</th>
-                            <th>FMA</th>
-                            <th>PPS</th>
-                            <th>Freq</th>
-                            <th>PPS</th>
-                            <th>Freq</th>
-                        </tr>
-                        <tr>
-                            <th>Shot Zone</th>
-                        </tr>
-                    </thead>
-                        
-                    <tbody>
-                        <tr>
-                            <td></td>
-                        </tr>
-                    </tbody>
+  const classes = useStyles();
 
-                </table>
-            </Card>
-        </div>
-    )
-}
+  return (
+    <div id="player-shot-chart-main">
+      <Card className={classes.root}>
+        <CardHeader title="Field Goal Breakdown" subheader="Player Shot Info" />
+        <table>
+          <thead>
+            <tr>
+              <th>Player</th>
+              <th>League</th>
+            </tr>
+            <tr>
+              <th>Shot Value</th>
+              <th>FGM</th>
+              <th>FMA</th>
+              <th>PPS</th>
+              <th>Freq</th>
+              <th>PPS</th>
+              <th>Freq</th>
+            </tr>
+            </thead>
+            {props.data ? (
+              Object.entries(props.data.shotValue).map((type) => {
+                <tr key={type[0]}>
+                  <td>{type[0]}</td>
+                  {type[1].map((value) => (
+                    <td key={value}>{type[1].value}</td>
+                  ))}
+                </tr>;
+              })
+            ) : (
+              <table id="shot-chart-layout">
+                <tbody>
+                  <tr>
+                    <th>3 Pt Shots</th>
+                  </tr>
+                  <tr>
+                    <th>2 Pt Shots</th>
+                  </tr>
+                  <tr>
+                    <th>Total</th>
+                  </tr>
+                </tbody>
+              </table>
+            )}
+            <tr>
+              <th>Shot Zone</th>
+            </tr>
+          
 
-export default ShotChart
+          <tbody>
+            <tr>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </Card>
+    </div>
+  );
+};
+
+export default ShotChart;
