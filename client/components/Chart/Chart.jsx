fcd3d8555752f70308150e4a609293a9bafd0cd3
Aram <aram_martin@alumni.brown.edu>
2021-07-05 18:17:38 -0500

    ADD: m-ui table header --- add tbody too

--- client/components/Chart/Chart.jsx
+++ client/components/Chart/Chart.jsx
@@ -1,55 +1,102 @@
-import React from 'react'
+import React from "react";
+import { Table, TableBody, TableCell, TableHead, TableRow } from "@material-ui/core";
+
 
 const Chart = ({ shotTypes, shotZones }) => {
+  const typeHeaders = ["Shot Type", "Made", "Missed", "Total", "FG%"];
+//   const zoneHeaders = ['', 'made', 'total', 'frequency', 'FG%']
+  const types = ["2", "3", "Total"];
+//   const zones = ["Restricted Area", "Paint", "Midrange", "ABT3", "Left Corner", "Right Corner", "Backcourt"]
 
-    const typeHeaders = ['', 'made', 'missed', 'total', 'FG%']
-    const types = ['2', '3', 'Total']
+  console.log(shotTypes);
+  return (
+    <div>
+        <Table aria-label='shot type table'>
+            <TableHead>
+                <TableRow>
+                    {typeHeaders.map((h, i) => (
+                        <TableCell key={i}>{h}</TableCell>
+                    ))}
+                </TableRow>
+            </TableHead>
 
-    console.log(shotTypes, shotZones)
-    return (
-        
-        <div>
-            
-            <table>
-                <thead>
-                    <tr>{typeHeaders.map((h, i) => <th key={i}>{h}</th>)}</tr>
-                </thead>
-                <tbody>
-                    {shotTypes ?
-                     Object.keys(shotTypes).map((k, i) => {
-                        let data = shotTypes[k]
-                        return (
-                            <tr key={i}>
-                                <td>{k}</td>
-                                <td>{data[0]}</td>
-                                <td>{data[1]}</td>
-                                <td>{data[2]}</td>
-                                <td>{data[3]}</td>
-                            </tr>
-                        )
-                    })
-                :   (
-                    types.map((k, i) => {
-                        return (
-                            <tr key={i}>
-                                <td>{k}</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
-                                <td>0</td>
-
-                            </tr>
-                        )
-                    })
+        </Table>
+      {/* <table>
+        <thead>
+          <tr>
+            {typeHeaders.map((h, i) => (
+              <th key={i}>{h}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {shotTypes
+            ? Object.keys(shotTypes).map((k, i) => {
+                let data = shotTypes[k];
+                return (
+                  <tr key={i}>
+                    <td>{k}</td>
+                    <td>{data[0]}</td>
+                    <td>{data[1]}</td>
+                    <td>{data[2]}</td>
+                    <td>{data[3]}</td>
+                  </tr>
+                );
+              })
+            : types.map((k, i) => {
+                return (
+                  <tr key={i}>
+                    <td>{k}</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
                     
-                    
-                )    
-                    
-                }
-                </tbody>
-            </table>
-        </div>
-    )
-}
+                  </tr>
+                );
+              })}
+        </tbody>
+      </table> */}
 
-export default Chart
+      {/* <table>
+          <thead>
+              <tr>
+                  {zoneHeaders.map((h, i) => {
+                      <th key={i}>{h}</th>
+                  })}
+              </tr>
+          </thead>
+          <tbody>
+          {shotZones
+            ? Object.keys(shotZones).map((k, i) => {
+                let data = shotZones[k];
+                return (
+                  <tr key={i}>
+                    <td>{k}</td>
+                    <td>{data[0]}</td>
+                    <td>{data[1]}</td>
+                    <td>{data[2]}</td>
+                    <td>{data[3]}</td>
+                    <td>{data[4]}</td>
+                  </tr>
+                );
+              })
+            : zones.map((k, i) => {
+                return (
+                  <tr key={i}>
+                    <td>{k}</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                  </tr>
+                );
+              })}
+
+          </tbody>
+      </table> */}
+    </div>
+  );
+};
+
+export default Chart;
