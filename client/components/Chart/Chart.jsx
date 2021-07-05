ea32e69393c285ed7db8038108020d3e539e1326
Aram <aram_martin@alumni.brown.edu>
2021-07-05 18:44:20 -0500

    ADD: m-ui shot type table

--- client/components/Chart/Chart.jsx
+++ client/components/Chart/Chart.jsx
@@ -1,26 +1,63 @@
 import React from "react";
-import { Table, TableBody, TableCell, TableHead, TableRow } from "@material-ui/core";
-
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@material-ui/core";
 
 const Chart = ({ shotTypes, shotZones }) => {
   const typeHeaders = ["Shot Type", "Made", "Missed", "Total", "FG%"];
-//   const zoneHeaders = ['', 'made', 'total', 'frequency', 'FG%']
+  //   const zoneHeaders = ['', 'made', 'total', 'frequency', 'FG%']
   const types = ["2", "3", "Total"];
-//   const zones = ["Restricted Area", "Paint", "Midrange", "ABT3", "Left Corner", "Right Corner", "Backcourt"]
+  //   const zones = ["Restricted Area", "Paint", "Midrange", "ABT3", "Left Corner", "Right Corner", "Backcourt"]
 
   console.log(shotTypes);
   return (
     <div>
-        <Table aria-label='shot type table'>
-            <TableHead>
-                <TableRow>
-                    {typeHeaders.map((h, i) => (
-                        <TableCell key={i}>{h}</TableCell>
-                    ))}
-                </TableRow>
-            </TableHead>
+      <Table aria-label="shot type table">
+        <TableHead>
+          <TableRow>
+            {typeHeaders.map((h, i) => (
+              <TableCell key={i}>{h}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
 
-        </Table>
+        <TableBody>
+          {shotTypes
+            ? Object.keys(shotTypes).map((k, i) => {
+                let data = shotTypes[k];
+                
+
+                return (
+                  <TableRow key={k}>
+                    <TableCell component="th" scope="row">
+                      {types[i]}
+                    </TableCell>
+                    <TableCell>{data[0]}</TableCell>
+                    <TableCell>{data[1]}</TableCell>
+                    <TableCell>{data[2]}</TableCell>
+                    <TableCell>{data[3]}</TableCell>
+                  </TableRow>
+                );
+              })
+            : types.map((k, i) => {
+                return (
+                  <TableRow key={k}>
+                    <TableCell component="th" scope="row">
+                      {types[i]}
+                    </TableCell>
+                    <TableCell>0</TableCell>
+                    <TableCell>0</TableCell>
+                    <TableCell>0</TableCell>
+                    <TableCell>0</TableCell>
+                  </TableRow>
+                );
+              })}
+        </TableBody>
+      </Table>
       {/* <table>
         <thead>
           <tr>
