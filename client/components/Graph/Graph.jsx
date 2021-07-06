3d0bb34f87b79ddaacb6c9893bbc8d943556f623
Aram <aram_martin@alumni.brown.edu>
2021-07-06 08:50:14 -0500

    ADD: scatter plot for made && missed shots --- invert court svg && integrate scatter onto it

--- client/components/Graph/Graph.jsx
+++ client/components/Graph/Graph.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import { Scatter } from 'react-chartjs-2'
 
-const Graph = ({ shots}) => {
-    console.log(shots)
+const Graph = ({ shots }) => {
+    const ctx = document.getElementById('court')
+
+    const shotGraph = shots ? (
+        <Scatter
+            options={{
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }}
+            data= {{
+                datasets: [
+                    {
+                        data: shots.madeShots.map((made) => made),
+                        label: 'Made',
+                        borderColor: '#3333ff',
+                        fill: false
+                    },
+                    {
+                        data: shots.missedShots.map((missed) => missed),
+                        label: 'Missed',
+                        borderColor: "rgba(255, 0, 0, 0.5)",
+                        fill: false
+                    }
+                ]
+            }}
+        />
+    ) : null
+
+    // console.log(shotGraph)
+
+
     return (
-        <h4>Graph</h4>
+        shotGraph
+       
+        
         
     )
 }
